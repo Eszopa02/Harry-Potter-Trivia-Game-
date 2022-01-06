b47bbf581ce341af692da591e47288038cade2f4
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2022-01-06 15:23:46 -0600

    Add files via upload
    Trivia Game 1.0 Created 09/27/20

---
+++ Hogwarts House Trivia Game/Hogwarts House Trivia Game/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+
+namespace TriviaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Game
+        {
+            string gameName = "Trivia Game";
+
+            public Game()
+            {
+                Title = gameName;
+                Console.WriteLine("Hello, welcom to Harry Potter Trivia, a game created by Eden Szopa");
+                Play();
+                Console.ReadKey();
+            }
+
+            public string Title { get; }
+
+            private void Play()
+            {
+
+            }
+
+        }
+
+        class Player
+        {
+            string name = "Player";
+            int score = 0;
+        }
+
+        class TriviaItem
+        {
+            public string questionOne = "What Hogwarts House is Harry Potter in? ";
+            public string answerOne = "Gryffindor";
+            public string questionTwo = "What is the name of the second Harry Potter book?";
+            public string anserTwo = "Harry Potter and the Chamber of Secrets";
+            public string questionThree = "What Hogwarts House is Draco Malfoy in?";
+            public string answerThree = "Slytherin";
+            public string questionFour = "How many books are in the Harry Potter series?";
+            public string questionFive = "True or False, there are many more Harry Potter movies than books.";
+            public string answerFive = "True";
+            public string questionSix = "How many Hogwarts houses are there?";
+            public string answerSix = "four";
+            public string questionSeven = "How many Horcruxes are there?";
+            public string answerSeven = "eight";
+        }
+    }
+}
